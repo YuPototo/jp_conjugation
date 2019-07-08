158c2d80ae82cbb85eeec855de62a7f4e7ffc568
Yu Qin <sirius.qinyu@gmail.com>
2019-07-08 17:52:47 +0800

    change data/data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,41 +1,41 @@
 export default [
   {
     id: 1,
-    title: "中国地理",
+    title: "动词て形",
     cards: [
       {
-        front: "中国的首都是哪里？",
-        back: "北京"
+        front: "開く",
+        back: "開いて"
       },
       {
-        front: "上海是哪条河的入海口？",
-        back: "长江"
+        front: "勝つ",
+        back: "勝って"
       },
       {
-        front: "中国最南的省份是？",
-        back: "海南"
+        front: "返す",
+        back: "返して"
       }
     ]
   },
   {
     id: 2,
-    title: "中国历史",
+    title: "ない形",
     cards: [
       {
-        front: "秦始皇在哪个朝代出生？",
-        back: "战国"
+        front: "怒る",
+        back: "怒らない"
       },
       {
-        front: "汉朝的开国皇帝是谁？",
-        back: "刘邦"
+        front: "慌てる",
+        back: "慌てない"
       },
       {
-        front: "三国武力最强的人是？",
-        back: "吕布"
+        front: "帰る",
+        back: "帰らない"
       },
       {
-        front: "刘备的结义兄弟是谁？",
-        back: "张飞、关羽"
+        front: "急ぐ",
+        back: "急がない"
       }
     ]
   }
